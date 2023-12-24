2784316768f53d18e2b7070473e0e2c2768a3a0d
Daehyun Kim <kimdhyungg@gmail.com>
2023-12-24 12:26:12 +0900

    feat(fish): begin using fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,87 +1,94 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-end
+    set -gx EDITOR nvim
+    set -gx VISUAL nvim
 
- starship init fish | source
+    starship init fish | source
 
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 
-set -x PATH $HOME/.local/bin $PATH
+    set -x PATH $HOME/.local/bin $PATH
 
-# Navigation aliases
+    # Navigation aliases
 
-alias ls "ls -a"
-alias ll "ls -la"
+    alias ls "ls -a"
+    alias ll "ls -la"
 
-function cz
-    set dir (zoxide query -l | fzf)
-    if test -n "$dir"
-        cd $dir
+    function cz
+        set dir (zoxide query -l | fzf)
+        if test -n "$dir"
+            cd $dir
+        end
     end
-end
 
-function cx
-    set dir (fdfind --type d | fzf)
-    if test -n "$dir"
-        cd $dir
+    function cx
+        set dir (fdfind --type d | fzf)
+        if test -n "$dir"
+            cd $dir
+        end
     end
-end
 
-# Find and Open aliases
+    # Find and Open aliases
 
-alias v "nvim"
+    alias v "nvim"
 
-function vx
-    set file (fdfind --type f | fzf)
-    if test -n "$file"
-        nvim $file
+    function vx
+        set file (fdfind --type f | fzf)
+        if test -n "$file"
+            nvim $file
+        end
     end
-end
 
-function vz
-    set file (/usr/bin/ls | head -n 1)
-    if test -n "$file"
-        nvim -- $file
+    function vz
+        set file (/usr/bin/ls | head -n 1)
+        if test -n "$file"
+            nvim -- $file
+        end
     end
+
+    alias gv "/mnt/c/Program\ Files/Neovide/neovide.exe --wsl"
+
+    # Config aliases
+
+    alias profile 'nvim ~/.config/fish/config.fish'
+
+    # Git aliases
+
+    alias gg "git-graph"
+    alias gl "git log --graph --pretty=format:\"%C(auto)%h %an%d%n%w(0,4,4)%<(50,trunc)%s\" --all"
+    alias gloga "git log --oneline --graph --all --decorate"
+    alias g "git"
+    alias gst "git status"
+    alias gsw "git switch"
+    alias lz "lazygit"
+    alias lg "lazygit"
+    alias gcz "~/.local/bin/cz"
+
+    # Utils aliases
+
+    alias cat "batcat"
+    alias bat "batcat"
+    alias which "which -a"
+
+    function mc
+        mkdir $argv
+        cd $argv
+    end
+
+    alias less "less -R" # with colors
+
+    # WSL aliases
+
+    alias ws "webstorm64.exe"
+    alias ii "explorer.exe"
+
+    function wh
+        set userprofile (cmd.exe /C "echo %USERPROFILE%" 2>/dev/null | tr -d '\r')
+        set wslpath (wslpath $userprofile)
+        cd $wslpath
+    end
+
+    fish_vi_key_bindings
 end
 
-# Config aliases
-
-alias profile 'nvim ~/.config/fish/config.fish'
-
-# Git aliases
-
-alias gg "git-graph"
-alias gl "git log --graph --pretty=format:\"%C(auto)%h %an%d%n%w(0,4,4)%<(50,trunc)%s\" --all"
-alias gloga "git log --oneline --graph --all --decorate"
-alias g "git"
-alias gst "git status"
-alias gsw "git switch"
-alias lz "lazygit"
-alias lg "lazygit"
-alias gcz "~/.local/bin/cz"
-
-# Utils aliases
-
-alias cat "batcat"
-alias bat "batcat"
-alias which "which -a"
-
-function mc
-    mkdir $argv
-    cd $argv
-end
-
-alias less "less -R" # with colors
-
-# WSL aliases
-
-alias ws "webstorm64.exe"
-alias ii "explorer.exe"
-
-function wh
-    set userprofile (cmd.exe /C "echo %USERPROFILE%" 2>/dev/null | tr -d '\r')
-    set wslpath (wslpath $userprofile)
-    cd $wslpath
-end
