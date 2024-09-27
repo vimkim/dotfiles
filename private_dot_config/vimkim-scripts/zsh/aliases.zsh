01fa598176f9d77736cd7bf959c3b936b79a7b4a
Daehyun Kim <kimdhyungg@gmail.com>
2024-09-27 21:43:05 +0900

    zali(minor): some web aliases

--- private_dot_config/vimkim-scripts/zsh/aliases.zsh
+++ private_dot_config/vimkim-scripts/zsh/aliases.zsh
@@ -554,6 +554,8 @@ alias prdiff='gh_pr_diff'
 
 alias mylog='$EDITOR $HOME/mylog/worklog-$(date +%Y-%m-%d).md'
 alias notion='open https://notion.so'
+alias gpt='open https://chatgpt.com'
+alias goo='open https://google.com'
 
 alias arxivtotext='arxiv2text'
 
