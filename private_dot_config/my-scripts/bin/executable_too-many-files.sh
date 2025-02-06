6fade8873f4eaa3fe2edad78bdd2d8e8e72b1755
Daehyun Kim <kimdhyungg@gmail.com>
2025-02-06 13:58:45 +0900

    minor(bin): too many files solve

---
+++ private_dot_config/my-scripts/bin/executable_too-many-files.sh
@@ -0,0 +1,4 @@
+# https://serverfault.com/a/1137212
+sudo sysctl -w fs.inotify.max_user_watches=2099999999
+sudo sysctl -w fs.inotify.max_user_instances=2099999999
+sudo sysctl -w fs.inotify.max_queued_events=2099999999
