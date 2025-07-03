66c4bf490b52d831adb1082d8fa90dae98c81838
Daehyun Kim <kimdhyungg@gmail.com>
2025-07-03 13:49:19 +0900

    fix(zellij attach): make ctrl c work

--- private_dot_config/my-scripts/bin/executable_attach-or-new.sh
+++ private_dot_config/my-scripts/bin/executable_attach-or-new.sh
@@ -2,9 +2,13 @@
 ZJ_SESSIONS=$(zellij list-sessions -s)
 NO_SESSIONS=$(echo "${ZJ_SESSIONS}" | wc -l)
 
-if [ "${NO_SESSIONS}" -ge 2 ]; then
-    zellij attach \
-        "$(echo "${ZJ_SESSIONS}" | fzf)"
+if [ "${NO_SESSIONS}" -ge 1 ]; then
+    chosen="$(echo "${ZJ_SESSIONS}" | fzf)"
+    if [ -z "$chosen" ]; then
+        echo "No session selected."
+        exit 0
+    fi
+    zellij attach "$chosen"
 else
     zellij -s "$(hostname)"
 fi
