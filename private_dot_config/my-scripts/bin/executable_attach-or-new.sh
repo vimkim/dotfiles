947c8005c35edac97065330bdb9f923fb510c6cf
Daehyun Kim <kimdhyungg@gmail.com>
2025-07-03 14:07:23 +0900

    fix(zellij attach): no session prints 1

--- private_dot_config/my-scripts/bin/executable_attach-or-new.sh
+++ private_dot_config/my-scripts/bin/executable_attach-or-new.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+
 ZJ_SESSIONS=$(zellij list-sessions -s)
-NO_SESSIONS=$(echo "${ZJ_SESSIONS}" | wc -l)
+
+if [ -z "${ZJ_SESSIONS}" ]; then
+    NO_SESSIONS=0
+else
+    NO_SESSIONS=$(printf "%s\n" "${ZJ_SESSIONS}" | wc -l)
+fi
+
+echo "${NO_SESSIONS} sessions found."
 
 if [ "${NO_SESSIONS}" -ge 1 ]; then
-    chosen="$(echo "${ZJ_SESSIONS}" | fzf)"
+    chosen="$(printf "%s\n" "${ZJ_SESSIONS}" | fzf)"
     if [ -z "$chosen" ]; then
         echo "No session selected."
         exit 0
