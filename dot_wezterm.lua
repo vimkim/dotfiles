d1a49c3ba7991c600b3f354405caf0325f093bf1
Daehyun Kim <kimdhyungg@gmail.com>
2025-10-24 23:07:55 +0900

    style(wezterm): Frappe -> macchiato

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,7 +4,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- config.color_scheme = "Catppuccin Mocha"
-config.color_scheme = "Catppuccin Frappe"
+-- config.color_scheme = "Catppuccin Frappe"
+config.color_scheme = "Catppuccin Macchiato"
 --  config.color_scheme = "Catppuccin Latte"
 
 -- config.font = wezterm.font("JetBrainsMonoNL Nerd Font Propo", { weight = "Bold" })
@@ -21,28 +22,28 @@ config.font_size = 15.0
 config.enable_tab_bar = false
 
 local xdg_config_home = os.getenv("HOME")
-config.background = {
-	-- first layer
-	{
-		-- Use an image as the background
-		source = {
-			-- File = xdg_config_home .. "/Pictures/arch-catppuccin-blurred.png", -- Provide the path to the image file
-			File = xdg_config_home .. "/.config/my-bg-imgs/arch-magenta.png", -- Provide the path to the image file
-		},
-
-		repeat_x = "NoRepeat",
-		horizontal_align = "Center",
-	},
-	-- second layer
-	{
-		source = {
-			Color = "rgba(48, 52, 70, 0.95)",
-		},
-		opacity = 0.85,
-		height = "100%",
-		width = "100%",
-	},
-}
+-- config.background = {
+-- 	-- first layer
+-- 	{
+-- 		-- Use an image as the background
+-- 		source = {
+-- 			-- File = xdg_config_home .. "/Pictures/arch-catppuccin-blurred.png", -- Provide the path to the image file
+-- 			File = xdg_config_home .. "/.config/my-bg-imgs/arch-magenta.png", -- Provide the path to the image file
+-- 		},
+--
+-- 		repeat_x = "NoRepeat",
+-- 		horizontal_align = "Center",
+-- 	},
+-- 	-- second layer
+-- 	{
+-- 		source = {
+-- 			Color = "rgba(48, 52, 70, 0.95)",
+-- 		},
+-- 		opacity = 0.85,
+-- 		height = "100%",
+-- 		width = "100%",
+-- 	},
+-- }
 
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = true
